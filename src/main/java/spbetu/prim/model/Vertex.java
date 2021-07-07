e1d2873b990f9d9802a743fe40fd496f38c6920f
Elizaveta Moskalenko <54900460+moskalenko9381@users.noreply.github.com>
2021-07-07 22:02:57 +0300

    Create Vertex.java

---
+++ src/main/java/spbetu/prim/model/Vertex.java
@@ -0,0 +1,65 @@
+package spbetu.prim.model;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class Vertex {  // класс ребра
+    int number;
+    HashMap<Vertex, Edge> edges;
+    private boolean visited;
+
+    Vertex(int number) {
+        this.number = number;
+        edges = new HashMap<>();
+        visited = false;
+    }
+
+    public void setVisited(boolean flag) {
+        visited = flag;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void vertexAddEdge(Vertex nextVertex, Edge edge) { // функция добавления ребра
+        edges.put(nextVertex, edge);
+    }
+
+    public Edge getMinimum() {  // возвращает ребро
+        Edge nextMinimumEdge = new Edge(Integer.MAX_VALUE, null, null);  /// ???? generic
+
+        Set<HashMap.Entry<Vertex, Edge>> set = edges.entrySet();
+
+        for (HashMap.Entry<Vertex, Edge> me : set) {
+
+            if (!me.getKey().isVisited() && (!me.getValue().isIncluded())) {
+                if (me.getValue().getWeight() < nextMinimumEdge.getWeight()) {
+                    nextMinimumEdge = me.getValue();  // возвращает ребро
+                }
+            }
+        }
+
+        return nextMinimumEdge;
+    }
+
+    public void forPreviousStep(Vertex to) {  // для отката назад, отметить, что ребро не посещали
+        Set<HashMap.Entry<Vertex, Edge>> set = edges.entrySet(); // словарь вершины
+        for (HashMap.Entry<Vertex, Edge> me : set) {
+            if (me.getKey().equals(to)) {
+                me.getValue().setIncluded(false);  // возвращает ребро
+            }
+        }
+    }
+
+    public void deleteEdgeFromDictionary(int indexVertexTo) {
+        Set<HashMap.Entry<Vertex, Edge>> set = edges.entrySet(); // словарь ребер вершины
+        for (HashMap.Entry<Vertex, Edge> edge : set) {
+            if (edge.getKey().number == indexVertexTo) { // нашли в словаре ребро ко второй вершине
+                edges.remove(edge.getKey());  // удаляем из словаря
+                break;
+            }
+        }
+    }
+
+}
